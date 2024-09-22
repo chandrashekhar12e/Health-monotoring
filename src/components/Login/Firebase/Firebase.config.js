6d84004a3f0ed103f3fa913e06f9adeb7b3d18f6
chandrashekhar <101011058+chandrashekhar12e@users.noreply.github.com>
2024-09-22 23:17:24 +0530

    Add files via upload

---
+++ src/components/Login/Firebase/Firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBneOJMD-ItFj8jSzWWrr1o42jMupZsUlg",
+  authDomain: "health-monitoring-app-b139c.firebaseapp.com",
+  projectId: "health-monitoring-app-b139c",
+  storageBucket: "health-monitoring-app-b139c.appspot.com",
+  messagingSenderId: "1083175634502",
+  appId: "1:1083175634502:web:47c74d6464b83411752c47",
+  measurementId: "G-M1FHPDZEFT"
+};
+
+// Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+export default firebaseConfig;
